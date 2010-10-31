bd38d4ef139f7fe2dcbf3a46c2ad63e3175ce55f
Charles Ma <chuckjma@gmail.com>
2010-10-31 20:50:56 +1100

    improved build script, updated readme

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 working_dir=`pwd`
 cd ..
+echo $working_dir
 
 version=`grep 'version' $working_dir/manifest.json | tr '"' '\n' |grep '[0-9]'`
 
-zip -r $working_dir/Simple-Usage-Meter-$version.zip $working_dir -x \*.git\* -x \*testdata\* 
+zip -r $working_dir/Simple-Usage-Meter-$version.zip $working_dir -x \*.git\*  $working_dir/testdata\*  $working_dir/docs\*  \*.zip
